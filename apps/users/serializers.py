82e571b710d89da1937ec5d9411d227dbee45fcb
santiago <ivsasafo30@hotmail.com>
2026-02-21 01:15:04 -0500

    feat: add users aserializers, views and urls

---
+++ apps/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import User, Profile
+
+class RegisterSerializer (serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators = [validate_password])
+    password2 = serializers.CharField(write_only = True)
+
+    class Meta :
+        model = User
+        fields = ['email','username',
+                  'password', 'password2']
+
+
+    def validate ( self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Las contraseñas no coinciden'})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        Profile.objects.create(user=user)
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'role']
+        read_only_fields = ['role']
+
+class ProfileSerializer (serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields=['user', 'phone', 'address', 'avatar']
+        
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, ProfileSerializer
+from .models import Profile
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
+    
+
