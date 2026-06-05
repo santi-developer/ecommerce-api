1d298bb61f27af172faeffa740ef1735c4cc486f
santi-developer <ivsasafo30@hotmail.com>
2026-06-05 00:40:08 -0500

    feat: auth context with login and logout state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import "./Navbar.css"
+import { useAuth } from "../context/AuthContext";
 
 function Navbar(){
+
+    const {isAuthenticated, logout} = useAuth()
 
     return(
     <nav className="navbar">
@@ -8,8 +11,16 @@ function Navbar(){
             <h1>Nexo</h1>
         </div>
         <div className="navbar-links">
-            <a href="/login">Iniciar sesión</a>
-            <a href="/register">Registrarse</a>
+             {isAuthenticated ? (
+                // Si está logueado
+                <button onClick={logout}>Cerrar sesión</button>
+            ) : (
+                // Si NO está logueado
+                <>
+                    <a href="/login">Iniciar sesión</a>
+                    <a href="/register">Registrarse</a>
+                </>
+            )}
         </div>
     </nav>
     )
